2a5bf452a5562418c720fb9f0746a6783031dcaf
ALIHAN <Dauletuly.Altair.09.10.02@gmail.com>
2022-11-04 11:41:07 +0600

    header fixed and any question

---
+++ src/app/any-questions/any-questions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-any-questions',
+  templateUrl: './any-questions.component.html',
+  styleUrls: ['./any-questions.component.scss']
+})
+export class AnyQuestionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
